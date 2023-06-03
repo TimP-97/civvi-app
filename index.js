674046b1df5a3ebc501aee424b99c8f5de82e91d
Tim Pierce <tim@Tims-MacBook-Pro-4.local>
2023-06-03 14:04:34 -0600

    FEAT: More get routes. New index styling. Add logo

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
+require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const app = express();
 const ejsLayouts = require('express-ejs-layouts');
 const methodOverride = require('method-override'); 
+const session = require('express-session');
+const flash = require('connect-flash');
+const passport = require('./config/ppConfig'); 
+const isLoggedIn = require('./middleware/isLoggedIn'); 
+let billType; 
+let billNumber; 
+let amendments; 
+let actions; 
+
+SECRET_SESSION = process.env.SECRET_SESSION;
+console.log('Secret session', SECRET_SESSION); 
+
 
 
 app.set('view engine', 'ejs');
@@ -10,16 +23,41 @@ app.use(ejsLayouts);
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 app.use(methodOverride('_method')); 
+app.use(flash()); 
+app.use(session({
+    secret: SECRET_SESSION,
+    resave: false,
+    saveUninitialized: true
+}));
+
+// add passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use((req, res, next) => {
+    console.log(res.locals); 
+    res.locals.alerts = req.flash(); 
+    res.locals.currentUser = req.user; 
+    next(); 
+})
 
 app.get('/', function (req, res) {
-    axios.get('https://api.spacexdata.com/v4/company')
+    axios.get('https://api.congress.gov/v3/bill?api_key=g34wvh7cMZqiTCkY4n3g39Se8vvZBrfTLC3lEg9I')
         .then(function (response) {
             // handle success
-            return res.render('index', { company: response.data });
+            return res.render('index', { bills: response.data });
         })
         .catch(function (error) {
             res.json({ message: 'Data not found. Please try again later.' });
         });
+});
+
+app.use('/auth', require('./controllers/auth'));
+
+// Add this below /auth controllers
+app.get('/profile', isLoggedIn, (req, res) => {
+    const { id, name, email } = req.user.get();
+    res.render('profile', { id, name, email });
 });
 
 
@@ -35,10 +73,65 @@ app.get('/bills', function (req, res) {
         });
 });
 
+app.get('/bills/118', function (req, res) {
+    axios.get('https://api.congress.gov/v3/bill/118?api_key=g34wvh7cMZqiTCkY4n3g39Se8vvZBrfTLC3lEg9I')
+        .then(function (response) {
+            // handle success
+            return res.json({ bills: response.data });
+        })
+        .catch(function (error) {
+            res.json({ message: 'Data not found. Please try again later.' });
+        });
+});
+
+app.get('/bills/118/:billtype', function (req, res) {
+    axios.get('https://api.congress.gov/v3/bill/118' + billType + '?api_key=g34wvh7cMZqiTCkY4n3g39Se8vvZBrfTLC3lEg9I')
+        .then(function (response) {
+            // handle success
+            return res.json({ bills: response.data });
+        })
+        .catch(function (error) {
+            res.json({ message: 'Data not found. Please try again later.' });
+        });
+});
+
+app.get('/bills/118/:billtype/:billnumber', function (req, res) {
+    axios.get('https://api.congress.gov/v3/bill/118' + billType + billNumber + '?api_key=g34wvh7cMZqiTCkY4n3g39Se8vvZBrfTLC3lEg9I')
+        .then(function (response) {
+            // handle success
+            return res.json({ bills: response.data });
+        })
+        .catch(function (error) {
+            res.json({ message: 'Data not found. Please try again later.' });
+        });
+});
+
+app.get('/bills/118/:billtype/:billnumber/amendments', function (req, res) {
+    axios.get('https://api.congress.gov/v3/bill/118' + billType + billNumber + amendments + '?api_key=g34wvh7cMZqiTCkY4n3g39Se8vvZBrfTLC3lEg9I')
+        .then(function (response) {
+            // handle success
+            return res.json({ bills: response.data });
+        })
+        .catch(function (error) {
+            res.json({ message: 'Data not found. Please try again later.' });
+        });
+});
+
+app.get('/bills/118/:billtype/:billnumber/actions', function (req, res) {
+    axios.get('https://api.congress.gov/v3/bill/118' + billType + billNumber + actions + '?api_key=g34wvh7cMZqiTCkY4n3g39Se8vvZBrfTLC3lEg9I')
+        .then(function (response) {
+            // handle success
+            return res.json({ bills: response.data });
+        })
+        .catch(function (error) {
+            res.json({ message: 'Data not found. Please try again later.' });
+        });
+});
 
 
 
-const PORT = process.env.PORT || 8000;
+
+const PORT = process.env.PORT || 3000;
 
 const server = app.listen(PORT, function () {
     console.log(`Server is running on PORT`, PORT);
--- models/user.js
+++ models/user.js
@@ -2,6 +2,7 @@
 const {
   Model
 } = require('sequelize');
+const bcrypt = require('bcryptjs');
 
 module.exports = (sequelize, DataTypes) => {
   class user extends Model {
@@ -14,6 +15,7 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
+
   user.init({
     name: {
       type: DataTypes.STRING,
@@ -45,6 +47,25 @@ module.exports = (sequelize, DataTypes) => {
     sequelize,
     modelName: 'user',
   });
+
+  user.addHook('beforeCreate', (pendingUser) => { // pendingUser is user object that gets passed to DB
+    // Bcrypt is going to hash the password
+    let hash = bcrypt.hashSync(pendingUser.password, 12); // hash 12 times
+    pendingUser.password = hash; // this will go to the DB
+  });
+
+  user.prototype.validPassword = function (typedPassword) {
+    let isCorrectPassword = bcrypt.compareSync(typedPassword, this.password); // check to see if password is correct.
+
+    return isCorrectPassword;
+  }
+
+  user.prototype.toJSON = function () {
+    let userData = this.get();
+    delete userData.password; // it doesn't delete password from database, only removes it. 
+
+    return userData;
+  }
 
   return user; // add functions above 
 };
