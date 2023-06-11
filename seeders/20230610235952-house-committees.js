448eb7f437f441b27d006ecc2ba856b74fa1eea6
Tim Pierce <tim@Tims-MacBook-Pro-4.local>
2023-06-10 18:01:33 -0600

    ADD: Other seed files to properly seed heroku

---
+++ seeders/20230610235952-house-committees.js
@@ -0,0 +1,56 @@
+'use strict';
+const axios = require('axios');
+
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await axios.get('https://api.congress.gov/v3/committee/house?format=json&offset=0&limit=250&api_key=g34wvh7cMZqiTCkY4n3g39Se8vvZBrfTLC3lEg9I')
+      .then(async function (response) {
+        const committees = response.data.committees.map(c => {
+          const result = {
+            chamber: c.chamber,
+            name: c.name,
+            systemCode: c.systemCode,
+            committeeTypeCode: c.committeeTypeCode,
+            bills: '',
+            billsCount: null,
+            history: [''],
+            communications: [''],
+            parent: JSON.stringify(c.parent),
+            url: c.url,
+            createdAt: new Date().toISOString(),
+            updatedAt: new Date().toISOString()
+          }
+          return result;
+        })
+        await queryInterface.bulkInsert('committees', committees, {});
+      })
+
+
+      .catch(function (error) {
+        console.log('ERROR!!!', error);
+      })
+  },
+
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+}
+
+
